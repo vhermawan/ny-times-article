b1bd7bbe08ae72e8f024189f7df728615288b6c9
Vicky Hermawan <107031434+vhermawan@users.noreply.github.com>
2023-05-12 01:42:50 +0700

    chore: change button style in gatcha page

--- src/features/gatcha/index.tsx
+++ src/features/gatcha/index.tsx
@@ -78,12 +78,7 @@ const GatchaPage: FC = () => {
                 Reedem your ticket and get more coint, if you never try you will never know!
               </p>
               <div className="mt-4 flex items-center justify-center gap-x-6">
-                <button
-                  onClick={handleButtonClick}
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Redeem
-                </button>
+                <Button onClick={handleButtonClick} text="Redeem" type="button" variant="info" />
               </div>
             </div>
             {showGacha && (
